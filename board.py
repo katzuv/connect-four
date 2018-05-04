05ad2461dfc4520e2b5ca1b1cc3e8fada80b0eb0
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-05-04 19:41:27 +0300

    Document board file

--- board.py
+++ board.py
@@ -1,24 +1,55 @@
 class Board(object):
+    """Class that represents a Connect Four board."""
     ROWS = 6
     COLUMNS = 7
     EMPTY = ' '
     TARGET = 4
 
     def __init__(self):
+        """Initialize a Connect Four board."""
         self.__rows = [[self.EMPTY for _ in xrange(self.COLUMNS)] for _ in xrange(self.ROWS)]
 
     def __str__(self):
-        return '\n'.join('\033[1m' + str(i) + '\033[0;0m' + ' ' + ' '.join(row) for i, row in enumerate(self.__rows, start=1)) + '\n' + \
-        '\033[1m' + '  1 2 3 4 5 6 7' + '\033[0;0m'
+        """
+        :return: a string describing the current board
+        :rtype: str
+        """
+        return '\n'.join('\033[1m' + str(i) + '\033[0;0m' + ' ' + ' '.join(row) for i, row in
+                         enumerate(self.__rows, start=1)) + '\n' + \
+               '\033[1m' + '  1 2 3 4 5 6 7' + '\033[0;0m'
 
     def is_full(self):
+        """
+        :return: whether the board is full.
+        :rtype: bool
+        """
         return all(self.EMPTY not in row for row in self.__rows)
 
     def move(self, column, player):
+        """
+        Make one move in the game and return whether player won the game.
+
+        :param column: column the player inputted
+        :type column: int
+        :param player: current player
+        :tyep player: str
+        :return: whether player won the game
+        :rtype: bool
+        """
         row = self.__insert(column, player)
-        return self.__is_winner(player, row, column)
+        return self.__is_winner(row, column, player)
 
     def __insert(self, column, player):
+        """
+        Insert a player's token into the board.
+
+        :param column: column number the player entered
+        :type column: int
+        :param player: current player
+        :type player: str
+        :return: row the token fell into
+        :rtype: int
+        """
         self.__raise_if_out_of_bounds(column)
         self.__raise_if_column_is_full(column)
         for i, row in enumerate(reversed(self.__rows)):
@@ -27,67 +58,155 @@ class Board(object):
             row[column] = player
             return i
 
-    def __is_winner(self, player, row, column):
-        return self.__is_winner_in_row(player, row, column) \
-               or self.__is_winner_in_column(player, row, column) \
-               or self.__is_winner_in_descending_diagonal(player, row, column) \
-               or self.__is_winner_in_ascending_diagonal(player, row, column)
+    def __is_winner(self, row, column, player):
+        """
+        :param row: row player inserted his last token into
+        :type row: int
+        :param column: column player inserted his last token into
+        :type column: int
+        :param player: current player
+        :type player: str
+        :return: whether player won the game
+        :rtype: bool
+        """
+        return self.__is_winner_in_row(row, column, player) \
+               or self.__is_winner_in_column(row, column, player) \
+               or self.__is_winner_in_descending_diagonal(row, column, player) \
+               or self.__is_winner_in_ascending_diagonal(row, column, player)
 
-    def __is_winner_in_row(self, player, row, column):
-        return self.__is_winner_in_sequence(player, self.__rows[row], column)
+    def __is_winner_in_row(self, row, column, player):
+        """
+        :param row: row player inserted his last token into
+        :type row: int
+        :param column: column player inserted his last token into
+        :type column: int
+        :param player: current player
+        :type player: str
+        :return: whether player won the game by creating a sequence of four tokens in a row
+        :rtype: bool
+        """
+        return self.__is_winner_in_sequence(self.__rows[row], column, player)
 
-    def __is_winner_in_column(self, player, row, column):
-        return self.__is_winner_in_sequence(player, [r[column] for r in self.__rows], row)
+    def __is_winner_in_column(self, row, column, player):
+        """
+        :param row: row player inserted his last token into
+        :type row: int
+        :param column: column player inserted his last token into
+        :type column: int
+        :param player: current player
+        :type player: str
+        :return: whether player won the game by creating a sequence of four tokens in a column
+        :rtype: bool
+        """
+        return self.__is_winner_in_sequence([r[column] for r in self.__rows], row, player)
 
-    def __is_winner_in_descending_diagonal(self, player, row, column):
+    def __is_winner_in_descending_diagonal(self, row, column, player):
+        """
+        :param row: row player inserted his last token into
+        :type row: int
+        :param column: column player inserted his last token into
+        :type column: int
+        :param player: current player
+        :type player: str
+        :return: whether player won the game by creating a sequence of four tokens in a descending diagonal
+        :rtype: bool
+        """
         if row >= column:
             difference = row - column
             return self.__is_winner_in_sequence(
-                player,
                 [self.__rows[i][i - difference]
                  for i in xrange(difference, min(self.ROWS, difference + self.COLUMNS))],
-                column)
+                column, player)
+
         difference = column - row
         return self.__is_winner_in_sequence(
-            player,
             [self.__rows[i - difference][i] for i in xrange(difference, min(difference + self.ROWS, self.COLUMNS))],
-            row)
+            row, player)
 
-    def __is_winner_in_ascending_diagonal(self, player, row, column):
-        sum = row + column
-        if sum < self.ROWS:
-            return self.__is_winner_in_sequence(player, [self.__rows[sum - i][i] for i in xrange(sum + 1)], column)
+    def __is_winner_in_ascending_diagonal(self, row, column, player):
+        """
+        :param row: row player inserted his last token into
+        :type row: int
+        :param column: column player inserted his last token into
+        :type column: int
+        :param player: current player
+        :type player: str
+        :return: whether player won the game by creating a sequence of four tokens in a ascending diagonal
+        :rtype: bool
+        """
+        addition = row + column
+        if addition < self.ROWS:
+            return self.__is_winner_in_sequence([self.__rows[addition - i][i] for i in xrange(addition + 1)], column,
+                                                player)
         return self.__is_winner_in_sequence(
-            player,
-            [self.__rows[sum - i][i] for i in xrange(sum - (self.ROWS - 1), min(sum + 1, self.COLUMNS))],
-            column - sum + (self.ROWS - 1))
+            [self.__rows[addition - i][i] for i in xrange(addition - (self.ROWS - 1), min(addition + 1, self.COLUMNS))],
+            column - addition + (self.ROWS - 1), player)
 
     @classmethod
-    def __is_winner_in_sequence(cls, player, sequence, index):
+    def __is_winner_in_sequence(cls, sequence, index, player):
+        """
+        :param sequence:
+        :type sequence: list
+        :param index: index of the latest token in the list
+        :param player:  current player
+        :type player: str
+        :return: whether player won the game by creating a sequence of four tokens
+        :rtype: bool
+        """
         target_slice = [player] * cls.TARGET
         return any(sequence[start:start + cls.TARGET] == target_slice
                    for start in xrange(max(0, index - (cls.TARGET - 1)), index + 1))
 
     @classmethod
     def __raise_if_out_of_bounds(cls, column):
+        """
+        Raise a ColumnOutOfBoundsError exception if the inputted column is out of board boundaries.
+
+        :param column: column number inputted
+        :type column: int
+        :raise: ColumnOutOfBoundsError
+        """
         if not 0 <= column < cls.COLUMNS:
             raise ColumnOutOfBoundsError(column)
 
     def __raise_if_column_is_full(self, column):
+        """
+        Raise a FullCommonError exception if the inputted column is already full.
+        :param column: column number inputted
+        :type column: int
+        :raise: FullColumnError
+        """
         if self.__rows[0][column] != self.EMPTY:
             raise FullColumnError(column)
 
-    def __is_winner(self, player, row, column):
-        raise NotImplementedError()
-
 
 class ColumnOutOfBoundsError(IndexError):
+    """
+    Exception for handling columns numbers inputted for columns that are out of board boundaries.
+    """
+
     def __init__(self, column):
+        """
+        Initialize a ColumnOutOfBoundsError exception.
+
+        :param column: column inputted for checking if it is out of board boundaries
+        :type column: int
+        """
         super(ColumnOutOfBoundsError, self).__init__()
         self.column = column
 
 
 class FullColumnError(Exception):
+    """
+    Exception for handling columns numbers fpr columns that are already full.
+    """
+
     def __init__(self, column):
+        """
+       Initialize a FullColumnError exception.
+
+       :param column: column inputted for checking if it is already full
+       :type column: int
+       """
         super(FullColumnError, self).__init__()
         self.column = column
